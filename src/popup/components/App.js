fa4e996cfcd735d3f10f8ad78390c7cdcac1ac52
Rie Miyazaki <rie.miyazaki@axa.co.jp>
2018-02-13 21:43:08 +0900

    Adapt to generate chrome auto script.

--- src/popup/components/App.js
+++ src/popup/components/App.js
@@ -4,45 +4,41 @@ import syntaxStyle from './syntaxStyle'
 import styles from './App.css'
 
 const App = props => (
-  <div>
-    <SyntaxHighlighter language='javascript' style={syntaxStyle}>
-      {`const Nightmare = require('nightmare')
-const nightmare = Nightmare({ show: true })
+    <div>
+        <SyntaxHighlighter language='javascript' style={syntaxStyle}>
+            {`const Chromy = require('chromy')
+let chromy = new Chromy({visible:true})
 
-nightmare
+chromy.chain()
 ${props.recording.reduce((records, record, i) => {
-  const { action, url, selector, value } = record
-  let result = records
-  if (i !== records.length) result += '\n'
+                const {action, url, selector, value} = record
+                let result = records
+                if (i !== records.length) result += '\n'
 
-  switch (action) {
-    case 'change':
-      result += `.type('${selector}', '${value}')`
-      break
-    case 'click':
-      result += `.click('${selector}')`
-      break
-    case 'goto':
-      result += `.goto('${url}')`
-      break
-    case 'reload':
-      result += `.refresh()`
-      break
-  }
+                switch (action) {
+                    case 'change':
+                        result += `.type('${selector}', '${value}')`
+                        break
+                    case 'click':
+                        result += `.click('${selector}')`
+                        break
+                    case 'goto':
+                        result += `.goto('${url}')`
+                        break
+                }
 
-  return result
-}, '')}
-.end()
-.then(function (result) {
-  console.log(result)
-})
-.catch(function (error) {
-  console.error('Error:', error);
-});`}
-    </SyntaxHighlighter>
+                return result
+            }, '')}
+    .evaluate(() => {
+        return document.querySelectorAll('*').length
+    })
+    .result((r) => console.log(r))
+    .end()
+    .then(() => chromy.close())`}
+        </SyntaxHighlighter>
 
-    <button className={styles.button} onClick={props.handleRestart}>Restart</button>
-  </div>
+        <button className={styles.button} onClick={props.handleRestart}>Restart</button>
+    </div>
 )
 
 App.displayName = 'App'
